5d366341c5a2bca22f75175a6cee994a2b9a3fc6
Thomas Donnelly <donnelly.thomas21@gmail.com>
2019-01-13 14:09:53 -0500

    Created model layer

---
+++ MVC/MVC/Model/AppleProduct.swift
@@ -0,0 +1,23 @@
+//
+//  AppleProduct.swift
+//  MVC
+//
+//  Created by Thomas Donnelly on 1/13/19.
+//  Copyright © 2019 Thomas Donnelly. All rights reserved.
+//
+
+import Foundation
+
+class AppleProduct {
+    var name: String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price: Double){
+        self.name = name
+        self.color = color
+        self.price = price
+    }
+}
+
+
